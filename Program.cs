c2fd37597e740c85a6661789d9db446d376cf06c
Heriberto Miguel <herimiguel84@hotmail.com>
2018-06-13 09:06:14 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace C_Assignments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i<= 255; i++){
+                Console.WriteLine(i);
+            }
+
+            for(int x = 1; x <= 100; x++){
+                if( ((x % 3) == 0) && ((x % 5) == 0) )
+                {
+                   Console.WriteLine("FizzBuzz");
+                }
+                // else if( x % 15 == 0){
+                //     Console.WriteLine("FizzBuzz");
+                // }
+
+                else if((x % 3) == 0){
+                    Console.WriteLine("Fizz");
+                }
+                else if((x % 5)== 0){
+                    Console.WriteLine("Buzz");
+                }
+
+            }
+        
+        }
+
+    }
+}
